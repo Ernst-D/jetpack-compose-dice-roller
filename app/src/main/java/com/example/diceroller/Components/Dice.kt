f4642a674168078d1b162e6cd5f0ef1cea9d7428
Ernst-D <ernst.dzerevanko@gmail.com>
2024-02-01 23:32:23 +0400

    add init state

--- app/src/main/java/com/example/diceroller/Components/Dice.kt
+++ app/src/main/java/com/example/diceroller/Components/Dice.kt
@@ -1,18 +1,21 @@
 package com.example.diceroller.Components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,10 +25,17 @@ import com.example.diceroller.R
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
     Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
-        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "Dice")
+        var result by remember {
+            mutableIntStateOf(1)
+        }
+
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription ="Roll: $result")
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(id = R.string.dice_text))
+        Button(onClick = {
+            println("Increment roll")
+            result++
+        }) {
+            Text(stringResource(id = R.string.dice_text)+" $result")
         }
     }
 }
